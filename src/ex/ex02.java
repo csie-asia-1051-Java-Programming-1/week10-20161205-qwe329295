38809d15aa5a07c1c3afca23a24eeb78857e267a
蕭佑誠 <105021028@live.asia.edu.tw>
2016-12-05 16:02:09 +0800

    ex

--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -2,13 +2,43 @@
 /*
  * Topic: 輸入所使用的度數，換算夏月及非夏月之電費金額。
  * Date: 2016/12/05
- * Author: 1050210XX 周永振老師
+ * Author: 105021028 蕭佑誠
  */
-
+import java.util.*;
 public class ex02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scn= new Scanner(System.in);
+		int d =scn.nextInt();
+		float s = 1,w=1;
+		if(d<=120){
+			s=(d*2.10f);
+			w=(d*2.10f);
+			System.out.println("夏月"+s);
+			System.out.println("非夏月"+w);
+		}else if(d>120 && d<=330){
+			s=((120*2.10f)+(d-120)*3.02f);
+			w=((120*2.10f)+(d-120)*2.68f);
+			System.out.println("夏月"+s);
+			System.out.println("非夏月"+w);
+		}else if(d>330 && d<=500){
+			s=((120*2.10f)+((330-120)*3.02f)+((d-330)*4.39f));    
+			w=((120*2.10f)+((330-120)*2.68f)+((d-330)*3.61f)); 
+			System.out.println("夏月"+s);
+			System.out.println("非夏月"+w);
+		}else if(d>500 && d<=700){
+			s=((120*2.10f)+((330-120)*3.02f)+((500-330)*4.39f)+(d-500)*4.97f);
+			w=((120*2.10f)+((330-120)*2.68f)+((500-330)*3.61f)+(d-500)*4.01f);
+			System.out.println("夏月"+s);
+			System.out.println("非夏月"+w);
+		}else if(d>700){
+			s=((120*2.10f)+((330-120)*3.02f)+((500-330)*4.39f)+((700-500)*4.97f)+(d-700)*5.63f);
+		    w=((120*2.10f)+((330-120)*2.68f)+((500-330)*3.61f)+((700-500)*4.01f)+(d-700)*4.50f);
+		    System.out.println("夏月"+s);
+			System.out.println("非夏月"+w);
+		}
+		
 
 	}
 
--- src/ex/ex03.java
+++ src/ex/ex03.java
@@ -2,13 +2,58 @@
 /*
  * Topic: 請設計 Person 類別 (姓名,性別,身高,體重)，使用者可以新增個人資料，每新增一筆就會列出目前的資料串有哪些人，而這些資料請用插入排序法依每一個人的 BMI 值由大到小排序。
  * Date: 2016/12/05
- * Author: 1050210XX 周永振老師
+ * Author: 105021028 蕭佑誠
  */
-
+import java.util.ArrayList;
+import java.util.Scanner;
 public class ex03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scn= new Scanner(System.in);
+		ArrayList<Integer> dataList = new ArrayList<Integer>();
+		String str="y";
+		int i=0,val=0;
+		boolean flag =false;
+		while(!(str.equals("n"))||str.equals(("n"))){
+			System.out.println("input a value");
+			val =scn.nextInt();
+			if(dataList.size()==0){
+				dataList.add(val);
+			}else if(dataList.size()==1){
+				if(dataList.get(i)>val){
+					dataList.add(val);
+			}else {
+				dataList.add(5,val);
+			}
+				
+			}else{
+				flag=true;i=0;
+				while(flag && i<dataList.size()-1){
+					if(i==0 && dataList.get(0)<val){
+						dataList.add(0,val);
+							flag=false;
+							dataList.add(i+1,val);
+						}else{
+							i++;
+						}
+					}
+				}
+			}
+			showListData(dataList);
+			System.out.println("Continue ?");
+			str =scn.next();
+			
+		}
+
+	private static void showListData(ArrayList<Integer> list) {
+		// TODO Auto-generated method stub
+		int size=list.size();
+		for(int i=0;i<size;i++){
+			System.out.println(list.get(i)+"\t");
+		}
+		System.out.println();
+	
 
 	}
 
